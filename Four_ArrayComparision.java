6a391baebc3e68bde66e81b611ef0588da7ff940
Sirish41 <86859551+Sirish41@users.noreply.github.com>
2023-08-01 10:47:45 +0530

    Program4: Return all elements of an array which are greater than their preceeding elements. (Always count 1st number)

--- Four_ArrayComparision.java
+++ Four_ArrayComparision.java
@@ -4,6 +4,14 @@ public class Four_ArrayComparision {
     public static void main(String args[]){
         Scanner scan=new Scanner(System.in);
         int n=scan.nextInt();
+        if(n==0){
+            System.out.println(0);
+            System.exit(0);
+        }
+        else if(n==1){
+            System.out.println(1);
+            System.exit(0);
+        }
         int arr[]=new int[n];
         int result=1;
         for(int i=0; i<n; i++){
