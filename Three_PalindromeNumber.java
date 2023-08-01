5737b87f163cfe8f31eb35331d6b5c8657a1a57c
Sirish41 <86859551+Sirish41@users.noreply.github.com>
2023-08-01 10:33:33 +0530

    Third program

---
+++ Three_PalindromeNumber.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Three_PalindromeNumber {
+    public static void main(String args[]){
+        Scanner scan=new Scanner(System.in);
+        int num=scan.nextInt();
+        scan.close();
+        int rNum=0;
+        int numClone=num;
+        while(numClone!=0){
+            int digit=numClone%10;
+            rNum=rNum*10+digit;
+            numClone/=10;
+        }
+        if(num-rNum==0){
+            System.out.println(num + " is palindrome");
+        }
+        else
+            System.out.println(num + " is not palindrome");
+    }    
+}
