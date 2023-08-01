cbc22e41dd28a66353e16ec81cd88c3e13683b62
Sirish41 <86859551+Sirish41@users.noreply.github.com>
2023-08-01 11:43:32 +0530

    Program4: Array comparisions

--- Four_ArrayComparision.java
+++ Four_ArrayComparision.java
@@ -4,20 +4,12 @@ public class Four_ArrayComparision {
     public static void main(String args[]){
         Scanner scan=new Scanner(System.in);
         int n=scan.nextInt();
-        if(n==0){
-            System.out.println(0);
-            System.exit(0);
-        }
-        else if(n==1){
-            System.out.println(1);
-            System.exit(0);
-        }
-        int arr[]=new int[n];
-        int result=1;
+        int ref=0, result=0;
         for(int i=0; i<n; i++){
-            arr[i]=scan.nextInt();
-            if(i!=0 && arr[i]>arr[i-1]){
+            int num=scan.nextInt();
+            if(num>ref){
                 result++;
+                ref=num;
             }
         }
         scan.close();
