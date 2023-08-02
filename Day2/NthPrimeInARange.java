4a18a8e6ad421a33db662c4ffdcdd34397e39f18
Sirish41 <86859551+Sirish41@users.noreply.github.com>
2023-08-02 12:14:08 +0530

    ...

--- Day2/NthPrimeInARange.java
+++ Day2/NthPrimeInARange.java
@@ -4,6 +4,7 @@ public class NthPrimeInARange {
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
         int start=scan.nextInt(), end=scan.nextInt(), n=scan.nextInt();
+        scan.close();
         int flag=0;
         int i=start-1, count=0, nthPrime=start;
         while(count!=n && i<end){
